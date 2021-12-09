73cd919fc3a6cccf0b0a56904ff9f34f668ff433
DullSaurcer <kbatbouta66@gmail.com>
2021-12-09 14:26:45 -0800

    Action initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const { Octokit } = require("@octokit/action");
+const octokit = new Octokit();
+
+try {
+    // `who-to-greet` input defined in action metadata file    
+    const runId = core.getInput('run-id');
+    const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+
+    console.log(`get-artifact.js started...`);
+    console.log(`run id = ${runId}`);
+    const { artifacts } = await octokit.request('GET /repos/{owner}/{repo}/actions/runs/{run_id}/artifacts', {
+        owner: owner,
+        repo: repo
+      });
+
+    core.out
+    if (artifacts.total_count != 0) {
+      var urls = '';
+      for(var i = 0;i < artifacts.total_count;i++) {
+        urls += artifacts.artifacts[i].archive_download_url + '\n';        
+      }
+      core.setOutput("urls", urls);
+    } else {
+      core.setFailed(`no artifacts where found in /repos/${owner}/${repo}/actions/runs/${runId}/artifacts`);
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
